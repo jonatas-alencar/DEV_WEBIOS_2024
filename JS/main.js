804d467aeacae5983eea7a4943ce1c16c356fa95
jonatas-alencar <jonatashalencar@gmail.com>
2024-05-08 21:43:14 -0300

    variaveis

---
+++ JS/main.js
@@ -0,0 +1,54 @@
+console.log("Hello Word!")
+//quando trabalho com texto coloco o mesmo dentro de aspas duplas ou simples
+
+//variaveis
+//quero criar um esoaço de memoria do computador chamado _jhow e dizer que esse espaço na memoria ira receber o valor 8
+var _jhow = 8;
+var _jhow = "messi" //redeclarar uma variavel
+let letsgo = 4; //nao pode redeclarar
+console.log(letsgo); 
+
+ letsgo = 3;
+
+ console.clear()
+
+
+
+//const
+ const PI = 3.1415;
+ 
+
+ console.log(PI);
+
+ console.clear()
+ 
+ let NAN = 0/0;
+
+ console.log(NaN);
+
+ console.clear()
+
+ let SãoPaulo = "campeao de tudo";
+ // variavel são paulo recebeu uma String   
+
+ console.log("campeao" + "de" + "Tudo" )  
+ console.clear()
+
+ var numero1 = 2
+ var texto = "eu tenho"
+ var text2 = "Reais"
+
+ console.log(texto + numero1 + text2)   
+ //é possivel concatenar strings ou concatenar variaveis
+
+ console.clear()
+ //valores booleanos
+ var num1 = 1
+ var num2 = 1
+ //num 1 é igual a num 2 = true
+ console.log(num1 == num2)  
+
+ var teste;
+  console.log(teste)
+ 
+
