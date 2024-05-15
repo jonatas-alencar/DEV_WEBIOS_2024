d672a68e6f85bc3c9134d5a02cdc33fe6aa36297
jonatas-alencar <jonatashalencar@gmail.com>
2024-05-14 21:41:02 -0300

    Default

---
+++ JS/JS-04-EXERCICIOSALA/main.js
@@ -0,0 +1,55 @@
+var numero1 = 6
+var numero2 = 4
+
+
+
+// if(numero1 > numero2){
+//     console.log("numero1 é maior que número2")
+// }else {
+//     console.log("numero1 é menor que numero2")
+// }
+
+numero1 > numero2 ? console.log("numero1 é maior") : console.log("numero1 é menor")
+
+console.clear()
+
+//se for janeiro = verao / junho / inverno / outubro / outono
+
+let mes = "janeiro"
+if(mes == "janeiro"){
+    console.log("é verao")
+}
+else if(mes == "junho"){
+    console.log("é outono")
+    }
+    else if(mes == "outubro"){
+        console.log("é primavera")
+    }
+    else{
+        console.log("é inverno")
+    }
+
+    console.clear()
+
+   let mes = "JANEIRO"
+   switch(mes){
+    case mes == "JANEIRO":
+    console.log("é verão");
+    break;
+
+    case "JUNHO":
+    console.log("é outono")
+    break;
+
+    case "OUTUBRO":
+    console.log("É primavera")
+    break;
+    default: 
+    console.log("é inverno")
+    break;
+   }
+
+
+
+
+   
---
+++ JS/js-04-condicional/main.js
@@ -0,0 +1,13 @@
+
+var todosdecracha = false
+//if >> se // else >>senao
+//true == true
+//true == false >> false
+
+if(todosdecracha == true){  
+console.log("todos recebem 10 em js")   
+
+}else{
+console.log("todos recebem  0 em js")
+
+}
