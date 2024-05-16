50c9ecfc6c41bf4bd6142d4391a4e84a6ea50566
jonatas-alencar <jonatashalencar@gmail.com>
2024-05-16 20:43:51 -0300

    Exercicio-03

---
+++ JS/Exercicio-Casa-03/main.js
@@ -0,0 +1,6 @@
+var string = "Estamos aprendendo javascript e seremos futuros Devs"
+
+console.log(string[19]);
+console.log(string.length);
+console.log(string.toUpperCase())
+console.log(string.substring(48))
