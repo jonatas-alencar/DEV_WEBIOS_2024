60e5370bc2a6c7f366746912b13636f02027df88
jonatas-alencar <jonatashalencar@gmail.com>
2024-05-15 21:41:36 -0300

    repetidores

---
+++ JS/JS-05-REPETIÇÃO/main.js
@@ -0,0 +1,21 @@
+//while
+//enquanto 0 <= 10 vou apresentar a mensagem 10 ainda é menor que 10
+// var contador = 10   
+
+// while(contador <= 10){
+//     console.log (contador + "0 ainda é menor que 10")
+//     //contador = 1 + 1
+//     //contador = 2
+//     debugger
+//     contador++; // contador = contador = 1
+var contador = 10
+do{
+    debugger
+    console.log  (contador + "ainda é menor que 10")
+    contador++;
+} while(contador < 10)
+
+for(var contador = 10; contador < 10 && contador >= 5 ; contador ++){
+    console.log  (contador + "ainda é menor que 10")
+}
+
